6e82166fb087aecfe5e32a0df53b8e755bc6b521
eoinmurray <amonecho1@gmail.com>
2016-08-22 17:29:49 +0200

    init parse .spc

---
+++ index.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Command line utility to convert .SPC files to .TXT
+
+author: Rohan Isaac
+"""
+from __future__ import division, absolute_import, unicode_literals, print_function
+import argparse
+import os
+import sys
+from lib.spc import File
+import ast
+import yaml
+
+
+def main(filepath):
+    filename_without_ext = os.path.splitext(filepath)[0]
+    delim = '\t'
+    f = File(filepath)
+
+    metadata = {
+        'xlabel': f.xlabel,
+        'ylabel': f.ylabel,
+        'zlabel': f.zlabel,
+        'exp_type': f.exp_type,
+    }
+
+    for bytes_ob in f.log_other:
+        s = bytes_ob.decode("utf-8")
+        if ':' in s:
+            x, y = s.split(':')
+            try:
+                y = ast.literal_eval(y.strip())
+            except:
+                y = y.strip()
+            metadata[x.strip()] = y
+
+    print (metadata)
+    f.write_file(filename_without_ext + '.txt', delimiter=delim)
+    with open(sys.argv[1] + '.yaml', 'w') as outfile:
+        outfile.write(yaml.dump(metadata, default_flow_style=False))
+
+if __name__ == "__main__":
+    main(sys.argv[1])
---
+++ readme.md
@@ -0,0 +1,29 @@
+# rinobot-parse-spc
+
+Parses spectral .spc files
+
+Outputs a .txt file with the x, y values of the spectrum
+and a .yaml file with the spectral metadata.
+
+## Installation
+
+Just download the zip of the package and unzip into your `rinobot-packages` folder.
+
+> [Download rinobot-parse-spc.zip](https://github.com/rinocloud/rinobot-parse-spc/archive/master.zip)
+
+- On Windows your `rinobot-packages` folder is in `My Documents`.
+- On Mac your `rinobot-packages` folder is in `Documents`.
+
+## Examples
+
+Output data will look like:
+
+```
+0.0 8.7
+1.4 2.4
+2.4 2.3
+3.3 3.5
+4.1 7.3
+...
+...
+```
